467278936e8cb779f8d06755096fed897f5243ab
ruchangheng <624813409@qq.com>
2020-01-21 06:55:44 +0000

    Heroku数据库参数修改

--- config/database.php
+++ config/database.php
@@ -82,12 +82,12 @@ return [
 //        ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => $db_config['host'],
-            'port' => env('DB_PORT', '5432'),
-            'database' => $db_config('database'),
-            'username' => $db_config('username'),
-            'password' => $db_config('password'),
+            'driver'   => 'pgsql',
+            'host'     => $db_config['host'],
+            'port'     => env('DB_PORT', '5432'),
+            'database' => $db_config['database'],
+            'username' => $db_config['username'],
+            'password' => $db_config['password'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
